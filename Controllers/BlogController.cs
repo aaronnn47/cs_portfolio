ac3da51915cd69a322ae6caeaa5cce622c11e02d
Aaron Kim <iamaaronkim@gmail.com>
2019-02-14 20:28:05 -0700

    first commit

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace portfolio.Controllers
+{
+    [Route("blog")]
+    public class BlogController : Controller
+    {
+        // GET: /<controller> 
+        public IActionResult Index()
+        {
+            var model = new portfolio.Models.Blog();
+            model.name = "aaron kim";
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/Blog.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace portfolio.Models
+{
+    public class Blog
+    {
+        public string name;
+    }
+}
